c13405ce91bf46f5c0c807f9ee883f34e33c2454
Tony Lagnada <deloyz@gmail.com>
2023-01-14 17:12:49 -0800

    LibStub update

--- Lib/Ace3/LibStub.lua
+++ Lib/Ace3/LibStub.lua
@@ -1,13 +1,9 @@
 ---@class LibStub
 local _LibStub = {
-    ---@param self LibStub
-    ---@param major string
-    ---@param minor number|string
-    NewLibrary = function (self, major, minor) end,
-
-    ---@param self LibStub
-    ---@param major string
-    ---@param minor number|string
-    GetLibrary = function (self, major, minor) end,
+    --- @type fun(self:LibStub, major:string, minor:number|string) : any
+    NewLibrary = {},
+    --- @type fun(self:LibStub, major:string, minor:number|string) : any
+    GetLibrary = {},
 }
 setmetatable(_LibStub, { __call = _LibStub.GetLibrary })
+
