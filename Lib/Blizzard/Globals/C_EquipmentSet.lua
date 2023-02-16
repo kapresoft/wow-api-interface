34a40d1186b55c12a30258e75b967d461025c2ee
Tony Lagnada <deloyz@gmail.com>
2023-02-16 14:22:23 -0800

    C_EquipmentSet, SecureExecution

---
+++ Lib/Blizzard/Globals/C_EquipmentSet.lua
@@ -0,0 +1,41 @@
+--- ## See: https://wowpedia.fandom.com/wiki/World_of_Warcraft_API#Equipment_Manager
+
+--- @class C_EquipmentSet
+local A = {
+
+    --- ### Set ID of an equipment set with the specified name. Starts at 0 (zero). Returns the set ID of an equipment set with the specified name, or nil if no sets with the specified name are currently saved.
+    --- ```
+    --- equipmentSetName = 'Boomkin'
+    --- setID = GetEquipmentSetID(equipmentSetName)
+    --- ```
+    --- @type fun(equipmentSetName:string) : number
+    GetEquipmentSetID = {},
+
+    --- ### Returns information about a saved equipment set.
+    ---```
+    --- name, iconFileID, setID, isEquipped, numItems, numEquipped, numInInventory, numLost, numIgnored = GetEquipmentSetInfo(1)
+    ---```
+    --- ### See: [https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetInfo](https://wowpedia.fandom.com/wiki/API_C_EquipmentSet.GetEquipmentSetInfo)
+    --- @type fun(equipmentSetId:number) : string, number, boolean, number, number, number, number, number
+    GetEquipmentSetInfo = {},
+
+    --- ### Returns whether any equipment sets can be used.
+    --- @type fun(: boolean
+    CanUseEquipmentSets = {},
+
+    --- @alias UseEquipmentSet fun(equipmentSetId:number) : boolean
+    --- ### Equips items from a specified equipment set (NO COMBAT)
+    --- @type fun(equipmentSetId:number) : boolean -- returns true if the set was equipped, nil otherwise. Failure conditions include invalid arguments, and engaging in combat.
+    UseEquipmentSet = {},
+}
+
+--- @class EquipmentSetCursor
+local E = {
+    type = 'equipmentset',
+    name = '<equipmentset-name>',
+}
+
+--[[-----------------------------------------------------------------------------
+sGlobal
+-------------------------------------------------------------------------------]]
+C_EquipmentSet = A
---
+++ Lib/Blizzard/Globals/SecureExecution.lua
@@ -0,0 +1,4 @@
+--- ### Check if combat lockdown restrictions are active.
+--- See: [https://wowpedia.fandom.com/wiki/API_InCombatLockdown](https://wowpedia.fandom.com/wiki/API_InCombatLockdown)
+--- @type fun(): boolean -- true if lockdown restrictions are currently in effect, false otherwise
+InCombatLockdown = {}
