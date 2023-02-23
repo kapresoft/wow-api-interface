6b3973d1211042af42edd7a65cc3d36bc427c282
Tony Lagnada <deloyz@gmail.com>
2023-02-23 14:50:15 -0800

    AceEvent updates

--- Lib/Ace3/AceEvent.lua
+++ Lib/Ace3/AceEvent.lua
@@ -1,4 +1,6 @@
----@class AceEvent
+--- @alias MessageHandlerFn fun(event:string, source:any, ...) | "function(evt, source, ...) end"
+--- @alias EventHandlerFn fun(event:string, source:any, ...) | "function(evt, source, ...) end"
+--- @class AceEvent
 local _AceEvent = {
     --- @param self AceEvent
     --- @param target any The object to embed AceEvent in
@@ -7,7 +9,7 @@ local _AceEvent = {
     --- Example: `AceEvent:RegisterMessage('Hello', function(evt, ...) end)`
     --- @param self AceEvent
     --- @param message string
-    --- @param callback fun(event:string, ...) : void The callback function to call when the message is triggered (funcref or method, defaults to a method with the event name)
+    --- @param callback MessageHandlerFn : void The callback function to call when the message is triggered (funcref or method, defaults to a method with the event name)
     RegisterMessage = function(self, message, callback, ...)  end,
     --- @param self AceEvent
     --- @param message string The message to unregister
@@ -15,7 +17,7 @@ local _AceEvent = {
 
     --- @param self AceEvent
     --- @param event string The event to register for
-    --- @param callback fun(event:string, ...) : void The callback function to call when the event is triggered (funcref or method, defaults to a method with the event name)
+    --- @param callback EventHandlerFn : void The callback function to call when the event is triggered (funcref or method, defaults to a method with the event name)
     --- Notes:
     --- - Additional arguments (...) An optional argument to pass to the callback function
     RegisterEvent = function(self, event, callback, ...)  end,
