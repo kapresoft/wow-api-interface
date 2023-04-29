4396dc7ba824fc1759a371bb28b7c580e0078737
Tony Lagnada <deloyz@gmail.com>
2023-04-29 16:14:04 -0700

    Colors

--- Lib/Blizzard/Aliases.lua
+++ Lib/Blizzard/Aliases.lua
@@ -4,7 +4,7 @@
 --- @alias DrawLayer string | "'BACKGROUND'" | "'BORDER'" | "'ARTWORK'" | "'OVERLAY'"  | "'HIGHLIGHT'"
 --- @alias SubDrawLayer number | "-8"| "-7"| "-6"| "-5" | "-4"| "-3"| "-2"| "-1" | "0"| "1"| "2"| "3" | "4"| "5"| "6" | "7"
 --- @alias NonNegativeNumber number | "0" | "1" | "2"
---- @alias HexColor string | "'FFAAFD0D0'"
+--- @alias HexColor string | "'FFEEA0A0'" | "'AARRGGBB'"
 --- @alias RGBColor number | "0.0" | "1.0"
 --- @alias Alpha number | "0.0" | "1.0"
 --- @alias RegionReference _Region | _ParentedObject | string | "'UIParent'" | "'FrameNameOrObj'"
