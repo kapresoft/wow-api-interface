b880823bc7f7a77605ace87313e67cca0807a92e
Tony Lagnada <deloyz@gmail.com>
2023-06-05 11:17:22 -0700

    Specialization update

--- Lib/Specialization.lua
+++ Lib/Specialization.lua
@@ -1,5 +1,5 @@
 --- #### See: [API_GetSpecialization](https://wowpedia.fandom.com/wiki/API_GetSpecialization)
---- Pre-Retail
+--- Retail
 --- @param isInspect BooleanOptional if true, return information for the inspected player.
 --- @param isPet BooleanOptional if true, return information for the player's pet (optional).
 --- @param specGroup SpecializationIndexOptional The index of a given specialization/talent/glyph group (1 for primary / 2 for secondary).
@@ -7,6 +7,7 @@
 function GetSpecialization(isInspect, isPet, specGroup) end
 
 --- #### See: [API_GetActiveTalentGroup](https://wowpedia.fandom.com/wiki/API_GetActiveTalentGroup)
+--- Pre-Retail
 --- @param isInspect BooleanOptional If true returns the information for the inspected unit instead of the player.
 --- @param isPet BooleanOptional If true returns the information for the inspected pet.
 --- @return SpecializationIndex The index of the current active talent group (1 for primary / 2 for secondary).
